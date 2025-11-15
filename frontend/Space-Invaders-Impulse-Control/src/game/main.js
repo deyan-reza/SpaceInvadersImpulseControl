9743df1bb1550b5d0864d181a5bab7846159ead6
Farhan Khan <fk27179@gmail.com>
2025-11-15 11:20:11 -0500

    frontend initialization

---
+++ frontend/Space-Invaders-Impulse-Control/src/game/main.js
@@ -0,0 +1,21 @@
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Game } from 'phaser';
+
+// Find out more information about the Game Config at:
+// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+const config = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scene: [
+        MainGame
+    ]
+};
+
+const StartGame = (parent) => {
+    return new Game({ ...config, parent });
+}
+
+export default StartGame;
